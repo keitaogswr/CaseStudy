2f0eff524536ceffc2952ad07e35072da100780c
Kuroda Masaki <rjsyc666@yahoo.co.jp>
2017-05-15 12:22:58 +0900

    更新内容は無いようです。

---
+++ UnityProject/Assets/Block.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Block : MonoBehaviour
+{
+    public GameObject Cube;
+    public string CubeName = "";
+    bool move;
+
+    //検索対象のマテリアル名
+    string[] CubeMats = { "Red", "Blue", "Green","Yellow" };
+
+    //追加用の色
+    //,"Purple"
+
+    GameMain Ctrl;
+
+	// Use this for initialization
+	void Start () {
+        move = true;
+        Ctrl = GameObject.Find("GameCtrl").GetComponent<GameMain>();
+        CreateBlock();
+	}
+
+    void Update()
+    {
+        int x, y;
+        if (move == true)
+        {
+            this.GetComponent<Rigidbody2D>().gravityScale = 10;
+            //Debug.Log(this.GetComponent<Rigidbody2D>().velocity);
+        }
+        else
+        {
+            this.GetComponent<Rigidbody2D>().gravityScale = 0;
+            //Debug.Log("Stop:" + this.GetComponent<Rigidbody2D>().velocity);
+        }
+
+        if (this.GetComponent<Rigidbody2D>().velocity.y < 0.05f)
+        {
+            x = ((int)(this.transform.position.x * 10.0f) / 7);
+        }
+    }
+
+    // Update is called once per frame
+    public void CreateBlock()
+    {
+        CubeName = CubeMats[Random.Range(0, CubeMats.Length)];
+
+        Material CubeMat = Resources.Load("Materials/" + CubeName) as Material;
+
+        this.GetComponent<Renderer>().material = CubeMat;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log(Mathf.Round(this.transform.position.y));
+    }
+
+    
+}
---
+++ UnityProject/Assets/GameMain.cs
@@ -0,0 +1,253 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct FIELD
+{
+    public bool Alive;
+    public bool Break;
+    public bool Lock;
+    public GameObject Cube;
+};
+
+public enum PHASE
+{
+    STAY = 0,
+    PUSH,
+    SERACH,
+    VANISH,
+    DROP,
+    GENERATE,
+};
+
+
+public enum CountType
+{
+    Y_Count = 0,
+    X_Count,
+};
+
+public class GameMain : MonoBehaviour {
+
+    public int gridWidth;       //オブジェクトの横配列量
+    public int gridHeight;      //オブジェクトの縦配列量
+    public GameObject Prefab;
+    // Use this for initialization
+
+    public float span;
+
+    public PHASE Phase;
+
+    //GameObject[,] Blocks = new GameObject[5, 10];
+
+    float time = 0.0f;
+
+    public FIELD[,] Field = new FIELD[7,10];
+
+    int BlockCounter = 0;
+
+    void Start() {
+        Phase = PHASE.STAY;
+        for (int x = -(gridWidth/2); x < gridWidth - (gridWidth / 2); x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                GameObject g = Instantiate(Prefab, new Vector3(x, y + 1, 0), Quaternion.identity) as GameObject;
+
+                //生成したオブジェクとの親にこのオブジェクトを設定
+                g.transform.parent = gameObject.transform;
+
+                SetCubeData(x + (gridWidth / 2), y, g);
+            }
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //int Cube_Cnt = 0;
+        int x, y;
+        //GameObject FirstObj, NextObj;
+
+        time += Time.deltaTime;
+        if (time > span)
+        {
+            time = 0;
+            for (x = 0; x < gridWidth; x++)
+            {
+                for(y = 0;y < gridHeight;y++)
+                {
+                    if(Field[x,y].Cube != null)
+                    {
+                        Debug.Log("Field[" + x + "," + y + "]:" + Field[x, y].Cube.GetComponent<Block>().CubeName);
+                    }
+                    else
+                    {
+                        Debug.Log("Field[" + x + "," + y + "]:キューブは存在しません");
+                    }
+                }
+            }
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            var tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var collition2d = Physics2D.OverlapPoint(tapPoint);
+
+            if(collition2d)
+            {
+                var hit = Physics2D.Raycast(tapPoint, -Vector2.up);
+                if(hit)
+                {
+                    Debug.Log("HitObj's Color = " + Field[(int)hit.collider.gameObject.transform.position.x + 3, (int)hit.collider.gameObject.transform.position.y - 1].Cube.GetComponent<Block>().CubeName);
+                }
+            }
+        }
+
+        switch(Phase)
+        {
+            case PHASE.STAY:
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("クリック！");
+                    Phase = PHASE.SERACH;
+                }
+                break;
+            case PHASE.PUSH:
+                break;
+            case PHASE.SERACH:
+                Debug.Log("サーチフェイズ");
+                for (x = 0; x < gridWidth; x++)
+                {
+                    for (y = 0; y < gridHeight; y++)
+                    {
+                        BlockCounter = 0;
+                        CountBlocks(x, y, CountType.X_Count);
+
+                        BlockCounter = 0;
+                        CountBlocks(x, y, CountType.Y_Count); 
+                    }
+                }
+                Phase = PHASE.VANISH;
+                break;
+            case PHASE.VANISH:
+                Debug.Log("ヴァニッシュフェイズ");
+                for (x = 0; x < gridWidth; x++)
+                {
+                    for (y = 0; y < gridHeight; y++)
+                    {
+                        if (Field[x, y].Break == true)
+                        {
+                            Destroy(Field[x, y].Cube, 1);
+                            Field[x, y].Alive = false;
+                            Field[x, y].Break = false;
+                            Debug.Log("X" + x + "Y" + y);
+                        }
+                    }
+                }
+                Phase = PHASE.DROP;
+                break;
+            case PHASE.DROP:
+                int DropCnt = 0;
+                int EmpCnt = 0;
+                for (x = 0; x < gridWidth; x++)
+                {
+                    for (y = 0; y < gridHeight; y++)
+                    {
+                        //フィールドに空きを見つけた
+                        if (Field[x, y].Alive == false)
+                        {
+                            DropCnt++;
+                            //空き領域より上にブロックが存在するか探す
+                            for(EmpCnt = y + 1;EmpCnt < gridHeight; EmpCnt++)
+                            {
+                                if(Field[x,EmpCnt].Alive == true)
+                                {
+                                    Debug.Log("列:" + x + "空き領域 :" + EmpCnt + "移動量:" + DropCnt);
+                                    break;
+                                }
+                                Debug.Log("列:" + x + "空き領域 :" + EmpCnt + "移動量:" + DropCnt);
+                                //空き領域までの数をカウント
+                                DropCnt++;
+                            }
+                            Debug.Log("抜けてる");
+                            //存在しない場合はカウントが不必要になるので0へ
+                            if (EmpCnt == gridHeight)
+                            {
+                                DropCnt = 0;
+                            }
+
+                            if(DropCnt > 0)
+                            {
+                                for(int i = EmpCnt;EmpCnt < gridHeight;EmpCnt++)
+                                {
+                                    Debug.Log("移動してる行：" + EmpCnt + "下がった量:" + DropCnt);
+                                    Field[x, EmpCnt - DropCnt] = Field[x, EmpCnt];
+                                    Field[x, EmpCnt].Cube = null;
+                                    Field[x, EmpCnt].Alive = false;
+                                }
+                                DropCnt = 0;
+                            }
+                        }
+                    }
+                }
+                Phase = PHASE.GENERATE;
+                break;
+            case PHASE.GENERATE:
+                Phase = PHASE.STAY;
+                break;
+        }       
+    }
+
+    private void GenerateCube()
+    {
+        for (int x = -(gridWidth / 2); x < gridWidth - (gridWidth / 2); x++)
+        {
+            GameObject g = Instantiate(Prefab, new Vector3(x * 0.7f, 0 + 10, 0), Quaternion.identity) as GameObject;
+
+            //生成したオブジェクとの親にこのオブジェクトを設定
+            g.transform.parent = gameObject.transform;
+        }
+    }
+
+    public void SetCubeData(int X, int Y,GameObject Obj)
+    {
+        Field[X, Y].Cube = Obj;
+        Field[X, Y].Alive = true;
+        Field[X, Y].Break = false;
+    }
+
+    private void CountBlocks(int X, int Y, CountType Type)
+    {
+        BlockCounter++;
+
+        if (Field[X, Y].Cube != null)
+        {
+            switch (Type)
+            {
+                case CountType.X_Count:
+                    if (X + 1 < gridWidth && Field[X + 1, Y].Cube != null && Field[X + 1, Y].Alive == true && Field[X + 1, Y].Cube.GetComponent<Block>().CubeName == Field[X, Y].Cube.GetComponent<Block>().CubeName)
+                    {
+                        CountBlocks(X + 1, Y, CountType.X_Count);
+                        Debug.Log("X_BlockCounter:" + BlockCounter);
+                    }
+                    break;
+                case CountType.Y_Count:
+                    if (Y + 1 < gridHeight && Field[X, Y + 1].Cube != null && Field[X, Y + 1].Alive == true && Field[X, Y + 1].Cube.GetComponent<Block>().CubeName == Field[X, Y].Cube.GetComponent<Block>().CubeName)
+                    {
+                        CountBlocks(X, Y + 1, CountType.Y_Count);
+                        Debug.Log("Y_BlockCounter:" + BlockCounter);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            if (BlockCounter >= 3)
+            {
+                Debug.Log("Field[" + X + "," + Y + "]:" + Field[X, Y].Cube.GetComponent<Block>().CubeName);
+                Field[X, Y].Break = true;
+            }
+        }
+    }
+}
