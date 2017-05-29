8736e990c072edd96fbcda22755b1314122fd373
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-05-29 11:12:07 +0900

    もう一回

---
+++ UnityProject/Assets/Resources/Script/PointManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointManager : MonoBehaviour {
+    private int point = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void AddPoint(int num)
+    {
+        point += num;
+    }
+
+    public void SubPoint(int num)
+    {
+        point -= num;
+    }
+
+    public int GetPoint()
+    {
+        return point;
+    }
+}
---
+++ UnityProject/Assets/Resources/Script/Point_UI_Manager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Point_UI_Manager : MonoBehaviour {
+
+    [SerializeField]
+    List<GameObject> numberPointList;
+
+    private GameObject pointManager;
+
+    // Use this for initialization
+    void Start () {
+        pointManager = GameObject.Find("PointManager");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        PointManager manager = pointManager.GetComponent<PointManager>();
+
+        int point = manager.GetPoint();
+        int workPoint;
+
+        for(int i = 0; i < numberPointList.Count; i++)
+        {
+            workPoint = point % 10;
+
+            Point number = numberPointList[i].GetComponent<Point>();
+            number.SetNumber(workPoint);
+
+            point = point / 10;
+        }
+    }
+}
