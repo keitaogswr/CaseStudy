2126de939a7d786d519eabc3c9448aa513954470
okada1192 <ths.okada.as@gmail.com>
2017-06-05 10:54:45 +0900

    タイトルのボタンをギアのスクリプトにPrefab突っ込めば自動生成できるようにしたよ、ランキング行くボタンスクリプト書いたよ

--- UnityProject/Assets/Resources/Script/buttonScript.cs
+++ UnityProject/Assets/Resources/Script/buttonScript.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class buttonScript : MonoBehaviour
 {
+    // 
+    public enum sceneNum
+    {
+        TITLE = 0,
+        GAME,
+        RANKING,
+    }
 
+    private string[] sceneName = {
+            "title",
+            "testscene",
+            "ranking",
+        };
+
+    // ゲーム開始ボタン
     public void ClickGameStart()
     {
         if (GetComponentInParent<titleGearScript>().GetRotFlag() == false)
         {
             Debug.Log("ゲームスタートボタンクリック");
-            GameObject.Find("Fade").GetComponent<fadeScript>().SetFade("testscene");
+            GameObject.Find("Fade").GetComponent<fadeScript>().SetFade(sceneName[(int)sceneNum.GAME]);
         }
     }
 
+    // ランキングボタン
+    public void ClickRanking()
+    {
+        if (GetComponentInParent<titleGearScript>().GetRotFlag() == false)
+        {
+            Debug.Log("ランキングボタン");
+            GameObject.Find("Fade").GetComponent<fadeScript>().SetFade(sceneName[(int)sceneNum.RANKING]);
+        }
+    }
+
+
+    // ゲーム修了ボタン
     public void ClickExit()
     {
         if (GetComponentInParent<titleGearScript>().GetRotFlag() == false)
