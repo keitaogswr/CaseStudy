bce4086d94bbc07ff3473b484936049588bf7e8f
zifuka <zifuka@gmail.com>
2017-06-13 12:35:39 +0900

    他シーンからの遷移処理、いくつかのデバッグ機能、PlayerPrefsにデータが無いときの処理を実装

--- UnityProject/Assets/Resources/Script/rankingScore.cs
+++ UnityProject/Assets/Resources/Script/rankingScore.cs
@@ -5,18 +5,35 @@ using UnityEngine.UI;
 
 public class rankingScore : MonoBehaviour {
 
-    public List<int> m_records = new List<int>();           // レコードデータ
-    public int m_myRecord;                                  // マイスコア
-    public bool m_isRecordTest;                             // レコードのテストデータを使うかどうか
+#if UNITY_EDITOR
+
+    [ Tooltip("テスト用のマイスコア") ]
+    public int m_testMyScore;
+
+    [ Tooltip("テスト用のマイスコアを使うか") ]
+    public bool m_isUseTestMyScore;
+
+    [ Tooltip("デフォルトのレコードを使うか") ]
+    public bool m_isUseDefaultRecord;
+
+    [ Tooltip("PlayerPrefsを初期化するか") ]
+    public bool m_isUseInitializePrefs;
+
+#endif
+
+    public List<int> m_recordsDefault = new List<int>();    // デフォルトレコードデータ
 
     public float m_interval;                                // 点滅周期
     public Color m_color;                                   // 通常色
     public Color m_FlashColor;                              // 点滅色
 
-    private Transform m_myScore;                            // マイスコアオブジェクト
-    private Transform m_rankInRecord;                       // ランクインしたレコードのオブジェクト
+    private Transform m_myScoreObj;                         // マイスコアオブジェクト
+    private Transform m_rankInRecordObj;                    // ランクインしたレコードのオブジェクト
     private float m_nextTime;                               // 点滅用カウンタ
     private bool m_flashing = false;                        // 光っている
+
+    static private int m_myScore = 0;                      // マイスコア
+    static private bool m_isMyScoreChanged = false;        // マイスコアが変更されたか
 
 
 
@@ -24,20 +41,44 @@ public class rankingScore : MonoBehaviour {
     {
         Debug.Log("Ranking Awaken");
 
+#if UNITY_EDITOR
+
+        if (m_isUseInitializePrefs)
+        {
+            PlayerPrefs.DeleteAll();
+        }
+
+#endif
+
         /* ランキングデータを読み込む */
 
-        if ( m_isRecordTest == false )
-        {
-            Transform child;
+        List<int> records = new List<int>( m_recordsDefault );
+        Transform recordObject = transform.FindChild("Records");
 
-            for (int i = 0; i < m_records.Count; i++)
+        Debug.Assert(records.Count == recordObject.childCount, "レコードサイズとレコードオブジェクトの数があっていません");
+
+        // PlayerPrefs内にデータがあれば読み込む
+        if (PlayerPrefs.HasKey(recordObject.GetChild(0).name))
+        {
+            Debug.Log("Loading Ranking Records.");
+
+            for ( int i = 0; i < records.Count; i++ )
             {
-                child = transform.GetChild(i);
-                m_records[i] = PlayerPrefs.GetInt(child.name);
+                Transform child = recordObject.GetChild(i);
+                records[i] = PlayerPrefs.GetInt(child.name);
             }
         }
 
-        InitializeRecord(); // オブジェクトへセット
+#if UNITY_EDITOR
+
+        if ( m_isUseDefaultRecord )
+        {
+            records = new List<int>(m_recordsDefault);
+        }
+
+#endif
+
+        InitializeRecord( ref records ); // オブジェクトへセット
 
         /*
          * ランキングデータの保存
@@ -45,15 +86,20 @@ public class rankingScore : MonoBehaviour {
          * 確実に保存しておく
          */
 
-        for (int i = 0; i < m_records.Count; i++)
+        for (int i = 0; i < records.Count; i++)
         {
-            Transform child = transform.GetChild(i);
-            PlayerPrefs.SetInt(child.name, m_records[i]);
+            Transform child = recordObject.GetChild(i);
+            PlayerPrefs.SetInt(child.name, records[i]);
         }
 
         PlayerPrefs.Save();
 
         Debug.Log("RankingRecord is saved.");
+    }
+
+    private void OnDestroy()
+    {
+        m_isMyScoreChanged = false;
     }
 
     // Use this for initialization
@@ -65,16 +111,16 @@ public class rankingScore : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if ( m_rankInRecord != null && m_nextTime < Time.time )
+        if ( m_rankInRecordObj != null && m_nextTime < Time.time )
         {
             /* 点滅処理 */
 
             m_flashing = !m_flashing;
 
-            SetRecordColor(m_myScore.FindChild("Digits"), m_flashing ? m_FlashColor : m_color);
+            SetRecordColor(m_myScoreObj.FindChild("Digits"), m_flashing ? m_FlashColor : m_color);
 
-            m_rankInRecord.FindChild("Rank").GetComponent<Image>().color = m_flashing ? m_FlashColor : m_color;
-            SetRecordColor(m_rankInRecord.FindChild("Digits"), m_flashing ? m_FlashColor : m_color);
+            m_rankInRecordObj.FindChild("Rank").GetComponent<Image>().color = m_flashing ? m_FlashColor : m_color;
+            SetRecordColor(m_rankInRecordObj.FindChild("Digits"), m_flashing ? m_FlashColor : m_color);
 
             // 次の周期へ
             m_nextTime += m_interval;
@@ -82,38 +128,47 @@ public class rankingScore : MonoBehaviour {
 	}
 
     // レコードオブジェクトの初期化
-    void InitializeRecord()
+    void InitializeRecord( ref List<int> records )
     {
         PointManager pointManager;
 
-        for (int i = 0; i < m_records.Count; i++)
+#if UNITY_EDITOR
+
+        if ( m_isUseTestMyScore )
         {
-            Transform child = transform.GetChild(i);
+            SetMyScore(m_testMyScore);
+        }
+
+#endif
+
+        for (int i = 0; i < records.Count; i++)
+        {
+            Transform child = transform.FindChild("Records").GetChild(i); ;
 
             // ランクイン判定
-            if (m_rankInRecord == null && m_myRecord > m_records[i])
+            if (m_isMyScoreChanged && m_rankInRecordObj == null && m_myScore > records[i])
             {
-                m_rankInRecord = child;
+                m_rankInRecordObj = child;
 
-                m_records.Insert(i, m_myRecord);
-                m_records.RemoveAt(m_records.Count - 1);
+                records.Insert(i, m_myScore);
+                records.RemoveAt(records.Count - 1);
 
                 Debug.Log("Rank In! : " + (i + 1));
             }
 
             pointManager = child.FindChild("PointManager").GetComponent<PointManager>();
-            pointManager.AddPoint(m_records[i]);
+            pointManager.AddPoint(records[i]);
 
             SetRecordColor(child.FindChild("Digits"), m_color);
         }
 
         // マイスコア
 
-        m_myScore = transform.FindChild("myScore");
-        pointManager = m_myScore.FindChild("PointManager").GetComponent<PointManager>();
-        pointManager.AddPoint(m_myRecord);
+        m_myScoreObj = transform.FindChild("myScore");
+        pointManager = m_myScoreObj.FindChild("PointManager").GetComponent<PointManager>();
+        pointManager.AddPoint(m_myScore);
 
-        SetRecordColor(m_myScore.FindChild("Digits"), m_color);
+        SetRecordColor(m_myScoreObj.FindChild("Digits"), m_color);
     }
 
     // レコードのスコアへ色をセット
@@ -124,5 +179,12 @@ public class rankingScore : MonoBehaviour {
             Image image = obj.GetComponent<Image>();
             image.color = color;
         }
+    }
+
+    // マイレコードへセット
+    static public void SetMyScore( int record )
+    {
+        m_myScore = record;
+        m_isMyScoreChanged = true;
     }
 }
