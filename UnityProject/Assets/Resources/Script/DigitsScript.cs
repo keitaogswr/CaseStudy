4933766ead5b9df2c7def1b771ba4cbb1a183d48
zifuka <zifuka@gmail.com>
2017-06-18 22:19:10 +0900

    Digitsをいちいち一個ずつサイズを変えるのをスクリプトで制御 頻繁に更新されていたのを修正

---
+++ UnityProject/Assets/Resources/Script/DigitsScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DigitsScript : MonoBehaviour {
+
+    public Vector2 size     = new Vector2( 1.0f, 1.0f );
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        foreach (RectTransform t in transform)
+        {
+            t.sizeDelta = size;
+        }
+    }
+}
