936c7f690d28899bf47f059f7692bc62cf7a49d2
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-06-23 17:31:25 +0900

    ui画像追加

--- UnityProject/Assets/Resources/Script/Timer.cs
+++ UnityProject/Assets/Resources/Script/Timer.cs
@@ -11,65 +11,76 @@ public class Timer : MonoBehaviour {
 
     private float Minutes = 0;
     private float Seconds = 0;
-    private float FPS = 60;
+    private const float FPS = 60;
     private bool timeOut = false;
+    private bool stop = false;
 
     // Use this for initialization
     void Start () {
         Minutes = GameTime / FPS;       //  分取得。
         Seconds = GameTime % FPS;       //  秒取得。
     }
-	
-	// Update is called once per frame
-	void Update () {
-        //  タイム減少。
-        if (timeOut == false && Pause.pausing == false) {
-            Seconds -= Time.deltaTime;
-            if (Seconds < 0.0f)
-            {       //  秒が0になったら分を削って秒を最大に
-                --Minutes;
-                if (Minutes < 0 && Seconds <= 0)
-                {
-                    timeOut = true;
-                    Pause.pausing = true;
-                }
-                else
-                {
-                    Seconds = FPS - 1.0f;
-                }
-            }
-        }
-        else if (timeOut == true)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                GameObject.Find("Fade").GetComponent<fadeScript>().SetFade("ranking");
-            }
-        }
 
-        if (timeOut == false)
+    // Update is called once per frame
+    void Update()
+    {
+        if (stop == false)
         {
-            int second = (int)Seconds;
-            int minutes = (int)Minutes;
-            int work;
-
-            for (int i = 0; i < numberPointList.Count; i++)
+            //  タイム減少。
+            if (timeOut == false && Pause.pausing == false)
             {
-                if (i < 2)
-                {    //  秒の計算
-                    work = second % 10;
-                    Point number = numberPointList[i].GetComponent<Point>();
-                    number.SetNumber(work);
-                    second = second / 10;
+                Seconds -= Time.deltaTime;
+                if (Seconds < 0.0f)
+                {       //  秒が0になったら分を削って秒を最大に
+                    --Minutes;
+                    if (Minutes < 0 && Seconds <= 0)
+                    {
+                        timeOut = true;
+                        Pause.pausing = true;
+                    }
+                    else
+                    {
+                        Seconds = FPS - 1.0f;
+                    }
                 }
-                else
-                {      //  分の計算
-                    work = minutes % 10;
-                    Point number = numberPointList[i].GetComponent<Point>();
-                    number.SetNumber(work);
-                    minutes = minutes / 10;
+            }
+            else if (timeOut == true)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GameObject.Find("Fade").GetComponent<fadeScript>().SetFade("ranking");
+                }
+            }
+
+            if (timeOut == false)
+            {
+                int second = (int)Seconds;
+                int minutes = (int)Minutes;
+                int work;
+
+                for (int i = 0; i < numberPointList.Count; i++)
+                {
+                    if (i < 2)
+                    {    //  秒の計算
+                        work = second % 10;
+                        Point number = numberPointList[i].GetComponent<Point>();
+                        number.SetNumber(work);
+                        second = second / 10;
+                    }
+                    else
+                    {      //  分の計算
+                        work = minutes % 10;
+                        Point number = numberPointList[i].GetComponent<Point>();
+                        number.SetNumber(work);
+                        minutes = minutes / 10;
+                    }
                 }
             }
         }
+    }
+
+    void SetTimeStop(bool timeStop)
+    {
+        stop = timeStop;
     }
 }
