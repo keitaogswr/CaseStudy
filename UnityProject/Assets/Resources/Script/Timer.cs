f4a77f0f9c1b3996739d1458b3b0ed7eff50a10b
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-06-27 18:39:16 +0900

    タイマーとか色々編集

--- UnityProject/Assets/Resources/Script/Timer.cs
+++ UnityProject/Assets/Resources/Script/Timer.cs
@@ -25,59 +25,66 @@ public class Timer : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-            //  タイム減少。
-            if (timeOut == false && Pause.pausing == false)
-            {
-                Seconds -= Time.deltaTime;
-                if (Seconds < 0.0f)
-                {       //  秒が0になったら分を削って秒を最大に
-                    --Minutes;
-                    if (Minutes < 0 && Seconds <= 0)
-                    {
-                        timeOut = true;
-                        Pause.pausing = true;
-                    }
-                    else
-                    {
-                        Seconds = FPS;
-                    }
+        //  タイム減少。
+        if (timeOut == false && Pause.pausing == false)
+        {
+            Seconds -= Time.deltaTime;
+            if (Seconds < 0.0f)
+            {       //  秒が0になったら分を削って秒を最大に
+                --Minutes;
+                if (Minutes < 0 && Seconds <= 0)
+                {
+                    timeOut = true;
+                }
+                else
+                {
+                    Seconds = FPS;
                 }
             }
-            else if (timeOut == true)
+        }
+        else if (timeOut == true)
+        {
+            if (gameMain.Phase == 0)
             {
                 if (Input.GetMouseButtonDown(0))
                 {
-                  　 PointManager pointManager = GameObject.Find("PointManager").GetComponent<PointManager>();
-                　   rankingScore.SetMyScore(pointManager.GetPoint());
-                  
+                    PointManager pointManager = GameObject.Find("PointManager").GetComponent<PointManager>();
+                    rankingScore.SetMyScore(pointManager.GetPoint());
+
                     GameObject.Find("Fade").GetComponent<fadeScript>().SetFade("ranking");
                 }
             }
+        }
 
-            if (timeOut == false)
+        if (timeOut == true && gameMain.Phase == 0)
+        {
+            Pause.pausing = true;
+        }
+
+        if (timeOut == false)
+        {
+            int second = (int)Seconds;
+            int minutes = (int)Minutes;
+            int work;
+
+            for (int i = 0; i < numberPointList.Count; i++)
             {
-                int second = (int)Seconds;
-                int minutes = (int)Minutes;
-                int work;
-
-                for (int i = 0; i < numberPointList.Count; i++)
-                {
-                    if (i < 2)
-                    {    //  秒の計算
-                        work = second % 10;
-                        Point number = numberPointList[i].GetComponent<Point>();
-                        number.SetNumber(work);
-                        second = second / 10;
-                    }
-                    else
-                    {      //  分の計算
-                        work = minutes % 10;
-                        Point number = numberPointList[i].GetComponent<Point>();
-                        number.SetNumber(work);
-                        minutes = minutes / 10;
-                    }
+                if (i < 2)
+                {    //  秒の計算
+                    work = second % 10;
+                    Point number = numberPointList[i].GetComponent<Point>();
+                    number.SetNumber(work);
+                    second = second / 10;
+                }
+                else
+                {      //  分の計算
+                    work = minutes % 10;
+                    Point number = numberPointList[i].GetComponent<Point>();
+                    number.SetNumber(work);
+                    minutes = minutes / 10;
                 }
             }
+        }
         
     }
 
