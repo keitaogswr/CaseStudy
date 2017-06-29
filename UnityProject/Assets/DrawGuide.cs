f79e8170cebcef64f9c45fc0178f9543e0fc9a16
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-06-29 16:48:43 +0900

    さらに追加分

--- UnityProject/Assets/DrawGuide.cs
+++ UnityProject/Assets/DrawGuide.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 using UnityEngine.UI;
 public class DrawGuide : MonoBehaviour {
 
+    private float Inertia = 0.08f;
+
     [SerializeField]
     private Text GuideText;
-    private string TutorialText;
+    [SerializeField]
+    private RectTransform Hukidasi;
+    
+    private bool ScaleTermination = false;
+    private Vector3 defScal;
+    private bool Reverse = false;
+    private bool ReverseOld = false;
 
     // Use this for initialization
     void Start () {
+        defScal = Hukidasi.localScale;
+        Hukidasi.localScale = new Vector3(0.0f, 0.0f, 0.0f);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //GuideText.text = TutorialText;
+        if (ScaleTermination)
+            Hukidasi.localScale += (defScal - Hukidasi.localScale) * Inertia;
+        else
+            Hukidasi.localScale += (new Vector3(0.0f, 0.0f, 0.0f) - Hukidasi.localScale) * Inertia;
     }
 
     public void SetText(string text)
     {
         GuideText.text = text;
+        ScaleTermination = false;
+        Hukidasi.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    public void SetReverse(bool reverse)
+    {
+        Reverse = reverse;
+        if (Reverse != ReverseOld)
+            defScal.x *= -1.0f;
+
+        ReverseOld = Reverse;
+    }
+
+    public void SetScaleTermination(bool scaleTermination)
+    {
+        ScaleTermination = scaleTermination;
     }
 }
--- UnityProject/Assets/TutorialCtrl.cs
+++ UnityProject/Assets/TutorialCtrl.cs
@@ -7,34 +7,57 @@ public class TutorialCtrl : MonoBehaviour {
 
     public GameObject Charcter1;
     public GameObject Charcter2;
+    public Canvas SelectCanvas;
 
     private DrawGuide m_DG1;
+    private DrawGuide m_DG2;
 
-    private enum STATE {
-        NONE = 0,
-        BOY,
+    private enum Scene
+    {
+        Game = 0,
+        Title,
+    };
+    private string[] Scene_Name =
+    {
+        "game",
+        "title",
+    };
+
+    private enum CHARCTER
+    {
+        BOY = 0,
         GIRL,
-        MAX
     };
 
     private string[] Tutorial_Text =
     {
+        "はさんでウィッチの世界へ\nようこそ！",
+        "これからこのゲームの世界に\nついて説明するね！",
         "これはスコア。\n薬石を消すたびに増えていくよ",
         "メニューを押すとゲームが一時停止して、メニュー画面が開くよ",
         "薬石を3つ揃えて薬の素材にしよう",
         "薬石を3つ揃えるときえるよ♪",
         "タップしたところに薬石が挟み込まれるよ",
         "連鎖するとゲージがたまるよ♪",
-        "次の薬石が確認できるよ"
+        "ここで次の薬石が確認できるよ"
     };
 
-    private STATE state;
+    private CHARCTER[] Responsible = { CHARCTER.BOY,
+                                       CHARCTER.BOY,
+                                       CHARCTER.BOY,
+                                       CHARCTER.BOY,
+                                       CHARCTER.BOY,
+                                       CHARCTER.GIRL,
+                                       CHARCTER.GIRL,
+                                       CHARCTER.GIRL,
+                                       CHARCTER.BOY };
     private int page = 0;
 
     // Use this for initialization
     void Start () {
-        state = STATE.NONE;
         m_DG1 = Charcter1.GetComponent<DrawGuide>();
+        m_DG2 = Charcter2.GetComponent<DrawGuide>();
+        SelectCanvas.enabled = false;
     }
 	
 	// Update is called once per frame
@@ -42,11 +65,47 @@ public class TutorialCtrl : MonoBehaviour {
         //左クリックされたら
         if (page < Tutorial_Text.Length)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (page == 0 && GameObject.Find("Fade").GetComponent<fadeScript>().GetFadeMode() == 0)
             {
                 m_DG1.SetText(Tutorial_Text[page]);
+                m_DG1.SetReverse(false);
+                m_DG1.SetScaleTermination(true);
+                m_DG2.SetScaleTermination(false);
+                ++page;
+            }
+            else if (Input.GetMouseButtonDown(0))
+            {
+                if (Responsible[page] == CHARCTER.BOY)
+                {
+                    m_DG1.SetText(Tutorial_Text[page]);
+                    m_DG1.SetReverse(false);
+                    m_DG1.SetScaleTermination(true);
+                    m_DG2.SetScaleTermination(false);
+                }
+                else if (Responsible[page] == CHARCTER.GIRL)
+                {
+                    m_DG2.SetText(Tutorial_Text[page]);
+                    m_DG2.SetReverse(false);
+                    m_DG1.SetScaleTermination(false);
+                    m_DG2.SetScaleTermination(true);
+                }
+                
                 ++page;
             }
         }
+        else
+        {
+            SelectCanvas.enabled = true;
+        }
+    }
+
+    public void TransitionGame()
+    {
+        GameObject.Find("Fade").GetComponent<fadeScript>().SetFade(Scene_Name[(int)Scene.Game]);
+    }
+
+    public void TransitionTitle()
+    {
+        GameObject.Find("Fade").GetComponent<fadeScript>().SetFade(Scene_Name[(int)Scene.Title]);
     }
 }
