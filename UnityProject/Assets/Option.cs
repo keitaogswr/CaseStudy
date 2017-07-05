cc5cffc3b23226bfa467bafd523acc8441a8fd06
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-07-05 23:49:39 +0900

    音量調整追加

---
+++ UnityProject/Assets/Option.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option : MonoBehaviour {
+
+    private Canvas optionCanvas;
+
+	// Use this for initialization
+	void Start () {
+        optionCanvas = GameObject.Find("OptionCanvas").GetComponent<Canvas>();
+        optionCanvas.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClickOption()
+    {
+        optionCanvas.enabled = true;
+    }
+
+    public void Return()
+    {
+        optionCanvas.enabled = false;
+    }
+}
